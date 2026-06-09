df75dc7d9d659ca25957c4f5a563212405389188
mohamadbarzegar93 <mohamad.barzegar72@gmail.com>
2026-06-09 17:13:48 +0200

    Phase 5 Step 1: added pytest tests - 3 passing

---
+++ tests/test_api.py
@@ -0,0 +1,35 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import io
+from fastapi.testclient import TestClient
+from api.main import app
+
+client = TestClient(app)
+
+def test_health_check():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "message" in response.json()
+
+def test_predict_valid_file():
+    with open("data/genres_original/blues/blues.00000.wav", "rb") as f:
+        response = client.post("/predict", files={"file":f})
+    assert response.status_code == 200
+    data = response.json()
+    assert "genre" in data
+    assert "confidence" in data
+    assert 0<=data["confidence"]<=100
+    assert data["genre"] in [
+        "blues", "classical", "country", "disco",
+        "hiphop", "jazz", "metal", "pop", "reggae", "rock"
+    ]
+
+
+def test_predict_invalid_format():
+    fake_file = io.BytesIO(b"not audio data")
+    response = client.post(
+        "/predict",
+        files = {"file": ("test.txt", fake_file, "text/plain")}
+    )
+    assert response.status_code ==500
